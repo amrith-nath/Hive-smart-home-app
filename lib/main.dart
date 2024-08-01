3cf063bec525c65da91771e68b0c6d8a343d0457
amrith-nath <amrithnath@outlook.com>
2024-08-01 18:07:56 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/app.dart';
+import 'package:hive/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const HiveApp());
 }
